e01e3c8bf39e8c950e96fc5defa9f61547517177
mtkmr <m.takamura0520@gmail.com>
2021-08-25 23:41:40 +0900

    Initial Commit

---
+++ FirebaseSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseSample
+//
+//  Created by Masato Takamura on 2021/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '14.0'
+
+target 'FirebaseSample' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for FirebaseSample
+  pod 'Firebase/Auth'
+  pod 'Firebase/DynamicLinks'
+
+end
